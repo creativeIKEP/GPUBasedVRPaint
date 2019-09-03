6e66627b059a5d58ead5ddf34671f2dd094ca1d1
creativeIKEP <syunya.i.1021@gmail.com>
2019-09-03 12:50:00 +0900

    line paint

--- SimpleComputeShader/Assets/SimpleParticleSystem/SimpleParticleSystem.cs
+++ SimpleComputeShader/Assets/SimpleParticleSystem/SimpleParticleSystem.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace SimpleParticleSystem
@@ -20,6 +20,7 @@ namespace SimpleParticleSystem
         const int NUM_THREAD_X = 8; // スレッドグループのX成分のスレッド数
         const int NUM_THREAD_Y = 1; // スレッドグループのY成分のスレッド数
         const int NUM_THREAD_Z = 1; // スレッドグループのZ成分のスレッド数
+        const int NUM_LINE_POSITIONS = 4000;
 
         public ComputeShader SimpleParticleComputeShader; // パーティクルの動きを計算するコンピュートシェーダ
         public Shader SimpleParticleRenderShader;  // パーティクルをレンダリングするシェーダ
@@ -36,6 +37,7 @@ namespace SimpleParticleSystem
 
         ComputeBuffer particleBuffer;     // パーティクルのデータを格納するコンピュートバッファ 
         Material particleRenderMat;  // パーティクルをレンダリングするマテリアル
+        Queue<Vector4> linePositions;
 
         void Start()
         {
@@ -47,7 +49,7 @@ namespace SimpleParticleSystem
             {
                 pData[i].Velocity = Random.insideUnitSphere;
                 pData[i].Position = Random.insideUnitSphere;
-                pData[i].LifeTime = lifeTime;
+                pData[i].LifeTime = lifeTime + Random.Range(-lifeTime * 0.5f, lifeTime * 0.5f);
                 pData[i].TimeFromRePositioning = 0;
             }
             // コンピュートバッファに初期値データをセット
@@ -58,25 +60,55 @@ namespace SimpleParticleSystem
             // パーティクルをレンダリングするマテリアルを作成
             particleRenderMat = new Material(SimpleParticleRenderShader);
             particleRenderMat.hideFlags = HideFlags.HideAndDontSave;
+
+            linePositions = new Queue<Vector4>();
+            //linePositions.Enqueue(reInitPos.position);
+        }
+
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                // Vector3でマウス位置座標を取得する
+                var position = Input.mousePosition;
+                // Z軸修正
+                position.z = 10f;
+                // マウス位置座標をスクリーン座標からワールド座標に変換する
+                var pos = Camera.main.ScreenToWorldPoint(position);
+                if (linePositions.Count >= NUM_LINE_POSITIONS)
+                {
+                    linePositions.Dequeue();
+                }
+                linePositions.Enqueue(pos);
+            }
         }
 
 
         void OnRenderObject()
         {
             ComputeShader cs = SimpleParticleComputeShader;
+            int kernelId = cs.FindKernel("CSMain");
+            if (linePositions.Count <= 0)
+            {
+                return;
+            }
+
             // スレッドグループ数を計算
             int numThreadGroup = NUM_PARTICLES / NUM_THREAD_X;
-            // カーネルIDを取得
-            int kernelId = cs.FindKernel("CSMain");
             // 各パラメータをセット
             cs.SetFloat("_TimeStep", Time.deltaTime);
             //scs.SetVector("_Gravity", Gravity);
             //cs.SetFloats("_AreaSize", new float[3] { AreaSize.x, AreaSize.y, AreaSize.z });
-            cs.SetVector("_RootPos", reInitPos.position);
+            //cs.SetVector("_RootPos", reInitPos.position);
+            cs.SetInt("_LinePositionsNum", linePositions.Count);
+            
+            cs.SetVectorArray("_LinePostions", linePositions.ToArray());
             // コンピュートバッファをセット
             cs.SetBuffer(kernelId, "_ParticleBuffer", particleBuffer);
             // コンピュートシェーダを実行
             cs.Dispatch(kernelId, numThreadGroup, 1, 1);
+
 
             // 逆ビュー行列を計算
             var inverseViewMatrix = RenderCam.worldToCameraMatrix.inverse;
