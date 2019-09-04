2e6c38749d5c4cf870514f6227336fc445765d55
creativeIKEP <syunya.i.1021@gmail.com>
2019-09-04 18:37:36 +0900

    save

---
+++ VR-Painting/Assets/VRPaint/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GPUBasedTrails;
+
+public class InputManager : MonoBehaviour
+{
+    public TrailBrush trailBrush;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Vector3でマウス位置座標を取得する
+            var position = Input.mousePosition;
+            // Z軸修正
+            position.z = 10f;
+            // マウス位置座標をスクリーン座標からワールド座標に変換する
+            var pos = Camera.main.ScreenToWorldPoint(position);
+            TrailBrush.Input nodeInput = new TrailBrush.Input { pos = pos };
+            trailBrush.InputPoint(new List<TrailBrush.Input> { nodeInput });
+        }
+    }
+}
---
+++ VR-Painting/Assets/VRPaint/TrailRender.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+public class TrailRender : MonoBehaviour
+{
+    public Material _material;
+    GPUBasedTrails.TrailBrush _trails;
+
+
+    private void Start()
+    {
+        _trails = GetComponent<GPUBasedTrails.TrailBrush>();
+    }
+
+    void OnRenderObject()
+    {
+        _material.SetInt(GPUBasedTrails.TrailBrush.CSPARAM.NODE_NUM_PER_TRAIL, _trails.nodeNum);
+        _material.SetFloat(GPUBasedTrails.TrailBrush.CSPARAM.LIFE, _trails.life);
+        _material.SetBuffer(GPUBasedTrails.TrailBrush.CSPARAM.TRAIL_BUFFER, _trails.trailBuffer);
+        _material.SetBuffer(GPUBasedTrails.TrailBrush.CSPARAM.NODE_BUFFER, _trails.nodeBuffer);
+        _material.SetPass(0);
+
+        var nodeNum = _trails.nodeNum;
+        var trailNum = _trails.trailNum;
+        Graphics.DrawProceduralNow(MeshTopology.Points, nodeNum, trailNum);
+    }
+}
