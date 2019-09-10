1b9cd36fb6d38c4602282e8c5e4e5cb323a33ef4
creativeIKEP <syunya.i.1021@gmail.com>
2019-09-10 16:53:49 +0900

    setup steam VR

---
+++ VR-Painting/Assets/SteamVRControllerInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+
+public class SteamVRControllerInput : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Boolean gripAction;
+
+    public SteamVR_Action_Boolean trrigerClick;
+
+    public SteamVR_Action_Boolean padClick;
+    public SteamVR_Action_Boolean padTouch;
+    public SteamVR_Action_Vector2 padPosition;
+
+
+    void Update()
+    {
+        if (gripAction.GetState(hand))
+        {
+            Debug.Log("gripAction");
+        }
+
+        if (trrigerClick.GetState(hand))
+        {
+            Debug.Log("trrigerClick");
+        }
+        
+
+        if (padClick.GetState(hand))
+        {
+            Debug.Log("padClick");
+        }
+        if (padTouch.GetState(hand))
+        {
+            Debug.Log("padTouch");
+            Debug.Log(padPosition.GetAxis(hand));
+            Debug.Log(padPosition.GetAxisDelta(hand));
+        }
+    }
+}
